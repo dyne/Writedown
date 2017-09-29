211c40033f34a805ae30c111ca5328356ece376c
Jaromil <jaromil@dyne.org>
2017-09-29 11:19:48 +0200

    default system wide configurations
    still to be improved: TOC switch and add abstract. latter is not
    needed when rendering a file outside of a project.

---
+++ config.zsh
@@ -0,0 +1,17 @@
+# Writedown
+# generic configuration defaults for rendered files
+
+WRITEDOWN_TITLE="Writedown Example Book"
+WRITEDOWN_AUTHOR="$(id -un)"
+WRITEDOWN_AFFILIATION="Made with Writedown by Dyne.org"
+WRITEDOWN_DATE="$(date +'%A, %d %B %Y')"
+WRITEDOWN_CITSTYLE="harvard-kings-college-london"
+WRITEDOWN_FONTSIZE="14pt"
+WRITEDOWN_NRSEC="no"
+WRITEDOWN_TOC="yes"
+WRITEDOWN_TAGS="[comma, separated, words, between, square, brackets]"
+WRITEDOWN_BIB="references.bib"
+
+# Experimental features are commented below
+
+# WRITEDOWN_ZOTERO="no"
