9ca08ca0b02647f1700252c7374c5cbe55fb201b
Jaromil <jaromil@dyne.org>
2017-10-18 13:35:18 +0200

    latex template configuration option

--- config.zsh
+++ config.zsh
@@ -1,16 +1,33 @@
 # Writedown
 # generic configuration defaults for rendered files
 
-WRITEDOWN_TITLE="Writedown Example Book"
-WRITEDOWN_AUTHOR="$(id -un)"
-WRITEDOWN_AFFILIATION="Made with Writedown by Dyne.org"
-WRITEDOWN_DATE="$(date +'%A, %d %B %Y')"
-WRITEDOWN_CITSTYLE="harvard-kings-college-london"
-WRITEDOWN_FONTSIZE="14pt"
-WRITEDOWN_NRSEC="no"
-WRITEDOWN_TOC="yes"
+WRITEDOWN_TITLE="Writedown Example Article"
+WRITEDOWN_AUTHOR="Anonymous"
+WRITEDOWN_AFFILIATION="Dyne.org Foundation"
+WRITEDOWN_DATE="Wednesday 18 October 2017"
 WRITEDOWN_TAGS="[comma, separated, words, between, square, brackets]"
-WRITEDOWN_BIB="references.bib"
+
+# number for each section
+WRITEDOWN_NRSEC=no
+# table of contents
+WRITEDOWN_TOC=yes
+# bibliographic citation style (see writedown/citstyle)
+WRITEDOWN_CITSTYLE=harvard-kings-college-london
+# font size
+WRITEDOWN_FONTSIZE=14pt
+# bibtex file for bibliographic sources
+WRITEDOWN_BIB=views/references.bib
+# latex template (header and footer)
+WRITEDOWN_LATEX_TEMPLATE=views/template.tex
+
+# different formats as supported by pandoc.
+# to activate uncomment and fill, then use dash (-) as first argument
+# i.e: ./writedown/render -
+# WRITEDOWN_OUTPUT_FORMAT=epub
+# WRITEDOWN_OUTPUT_EXTENSION=epub
+
+# default pandoc base command
+# WRITEDOWN_PANDOC="pandoc --smart --standalone -f markdown
 
 # Experimental features are commented below
 
